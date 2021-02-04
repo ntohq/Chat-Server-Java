d9cb5375a19bacbdd80702da09e631b11bae608b
Wesley Ford <mangofordW@gmail.com>
2021-02-04 15:16:28 -0600

    Template Created

---
+++ Server/Main.java
@@ -0,0 +1,17 @@
+/********************************************************************************
+Written By: Wesley Ford (ntohq), and Seth Ivy (SethIvry)
+Date:       02-04-2021 
+Version:    0.001
+********************************************************************************
+Documentation:
+   This program host a chat room for others to connect to with special software
+   over a LAN (Local Area Network)
+********************************************************************************/
+
+class Main
+{
+   public static void main(String[] args)
+   {
+
+   }
+}
---
+++ Server/SocketHandler.java
@@ -0,0 +1,7 @@
+public class SocketHandler
+{
+   public static void main(String[] args)
+   {
+      
+   }
+}
